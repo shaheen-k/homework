cca8140ea7d670103d315958888b8dc4f1ad2257
Shaheen Khatua <shaheenkhatua@Shaheens-MacBook-Air.local>
2022-01-24 12:36:51 -0800

    done

---
+++ 11sumfac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Write a program that computes the running sum from 1..n
+# Also, have it compute the factorial of n while you're at it
+# No, you may not use math.factorial()
+# Use the same loop for both calculations
+
+n = 5
+sum = 5
+fac = 5
+for i in range(1, 5):
+	sum += i
+	fac *= i
+print(n, sum, fac)
+
+
+"""
+python3 11sumfac.py
+5 15 120
+"""
